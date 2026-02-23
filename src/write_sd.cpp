93bb77e587be0f94651b77933a186f3352a53ecc
buttonbugs <miller.chen2006@outlook.com>
2026-02-22 18:25:53 -0700

    Change the function sequence in write_sd.cpp

--- src/write_sd.cpp
+++ src/write_sd.cpp
@@ -5,37 +5,7 @@
 int file_id = 0;
 char file_name[13] = "data_000.csv";
 
-void init_sd() {
-    Serial.print("Initializing SD card...");
-    if (!SD.begin(SD_CARD_CS)) {
-        Serial.println("initialization failed!");
-        return;
-    }
-    Serial.println("initialization done.");
-    file_size_test(true);
-}
-
-void file_size_test(bool file_change = false) {
-    File dataFile = SD.open(file_name);
-    uint32_t file_size = dataFile.size();
-    dataFile.close();                   // Crucial: Always close to save data
-    while (file_size >= FILE_SIZE_LIMIT) {      // create another data file when the current file size exceeds FILE_SIZE_LIMIT
-        file_change = true;
-        file_id++;
-        sprintf(file_name, "data_%03d.csv", file_id);
-
-        // Get file size
-        File dataFile = SD.open(file_name);
-        file_size = dataFile.size();
-        dataFile.close();               // Crucial: Always close to save data
-    }
-    if (file_change) {
-        write_column_headers();
-    }
-    
-}
-
-void write_column_headers (){
+void write_column_headers(){
     File dataFile = SD.open(file_name, FILE_WRITE);
     dataFile.print('"Timestamp (ms)",');
     dataFile.print('"Ozone (PPB)",');
@@ -58,6 +28,36 @@ void write_column_headers (){
     dataFile.print('"Ultrasonic 3 (us)",');
     dataFile.print('\n');
     dataFile.close();               // Crucial: Always close to save data
+}
+
+void file_size_test(bool file_change = false) {
+    File dataFile = SD.open(file_name);
+    uint32_t file_size = dataFile.size();
+    dataFile.close();                   // Crucial: Always close to save data
+    while (file_size >= FILE_SIZE_LIMIT) {      // create another data file when the current file size exceeds FILE_SIZE_LIMIT
+        file_change = true;
+        file_id++;
+        sprintf(file_name, "data_%03d.csv", file_id);
+
+        // Get file size
+        File dataFile = SD.open(file_name);
+        file_size = dataFile.size();
+        dataFile.close();               // Crucial: Always close to save data
+    }
+    if (file_change) {
+        write_column_headers();
+    }
+    
+}
+
+void init_sd() {
+    Serial.print("Initializing SD card...");
+    if (!SD.begin(SD_CARD_CS)) {
+        Serial.println("initialization failed!");
+        return;
+    }
+    Serial.println("initialization done.");
+    file_size_test(true);
 }
 
 void write_sd() {
