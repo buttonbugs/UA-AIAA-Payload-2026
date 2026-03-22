05acfb34561f0e8fcc8602c9d78cf7469d395c35
buttonbugs <miller.chen2006@outlook.com>
2026-03-22 14:25:04 -0700

    End testing

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,10 @@ void setup() {
     Wire.begin();       // Important! Initialize the I2C (Inter-Integrated Circuit) communication protocol
     Serial.begin(9600);
     delay(100);
-    // init_sd();
-    init_vibration_sensor();
+    init_sd();
 }
 
 void loop() {
-    // write_sd();
+    write_sd();
     delay(SAMPLE_ECHOTERVAL);
-    check_vibration_timeout();
-    Serial.print(vibration_period_1_ms);
-    Serial.println(vibration_period_2_ms);
 }
